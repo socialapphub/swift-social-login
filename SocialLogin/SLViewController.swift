69f08a8ca0bc5258943df2972642ffe5a08d16e0
PAOLO MUSOLINO <info@codeido.com>
2016-01-15 18:03:31 +0100

    Implementati metodi delegati dell'authentication manager nel view controller principale.

--- SocialLogin/SLViewController.swift
+++ SocialLogin/SLViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import FacebookSDK
+import FBSDKCoreKit
+import FBSDKLoginKit
+import TwitterKit
+import Fabric
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SLDelegate {
 
+    var authenticationManager:SLAuthenticationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       // self.authenticationManager.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+        //Facebook Delegates
+    func fbLoggedInUser(loginResult:FBSDKLoginManagerLoginResult){
+        
+    }
+    
+    func fbLoggedOutUser(){
+        
+    }
+    
+    func fbLoginViewError(loginResult:FBSDKLoginManagerLoginResult, error:NSError){
+        
+    }
+    
+    //Twitter Delegates
+    func twitterLoginButtonPressed(){
+        
+    }
+    
+    func twitterLoggedInUser(session:TWTRSession){
+        
+    }
+    
+    func twitterHandleError(error:NSError){
+        
+    }
+    
+    func twitterLoggedOutUser(){
+        
+    }
+    
+    //Native Login Delegates
+    func tapOnNativeLoginButton(){
+        
+    }
+    
+    func requestOfNativeLoginWithEmail(email:String, password:String){
+        
+    }
+    
+    //Native Registration Delegates
+    func tapOnNativeRegistrationButton(){
+        
+    }
+    
+    func requestOfNativeRegistrationWithEmail(email:String, password:String, name:String, surname:String){
+        
+    }
+    
 }
 
