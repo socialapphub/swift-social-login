a5f97dd20d0fde7beb338962622316a13dc37104
PAOLO MUSOLINO <info@codeido.com>
2016-01-19 12:24:41 +0100

    - Prima integrazione travis-ci - Eliminato test delle performance da SLSingletonTests

--- SocialLoginTests/SLSingletonTests.swift
+++ SocialLoginTests/SLSingletonTests.swift
@@ -58,11 +58,4 @@ class SLSingletonTests: XCTestCase {
         }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
